4fc60f8434907fad7ca2d686d828d46a8dba31c1
Meena Erian Awad Hanna <meena5.erian@gmail.com>
2017-02-09 02:12:03 +0200

    Update theme.js

--- siteCommon/theme.js
+++ siteCommon/theme.js
@@ -13,7 +13,7 @@ include('/SmarF/siteCommon/commonSiteHeader.html', document.head);
 include('/SmarF/siteCommon/commonSiteFooter.html', document.getElementsByClassName("paper")[0]);
 window.onload = function() {
   function lateOnLoad() { //Excute this function after loading everything including external parts.
-    document.getElementById('link-to-github').setAttribute("href", "https://github.com/meena-erian/SmarF/blob/master/" + document.URL.split('?')[0].split('/').slice(4).join('/'));
+    document.getElementById('link-to-github').setAttribute("href", "https://github.com/meena-erian/SmarF/tree/master/" + document.URL.split('?')[0].split('/').slice(4).join('/'));
   }
   var loopHandler = setInterval(function() {if(!numberOfActiveHTTPRequests) {clearInterval(loopHandler); lateOnLoad();} }, 100);
 }
